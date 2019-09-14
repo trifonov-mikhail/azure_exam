0eec89be7f16794c9269c7a0a122d3a2c1ce8083
Mikhail_Trifonov <Mikhail_Trifonov@epam.com>
2019-09-14 13:21:49 +0300

    s

--- create_group.sh
+++ create_group.sh
@@ -2,7 +2,7 @@
 RG_NAME=lab1_exam_ks
 LOCATION=eastus
 rgExists=$(az group exists --name RG_NAME)
-if[ $rgExists ] then
+if[ $rgExists -ge true ] then
 	az group delete --name $RG_NAME --yes
 fi
 az group create --name $RG_NAME --location $LOCATION
